4492918af831a802f0b0e57901fb1701b158ad04
clcriado0 <carlos.larriba@metadata.es>
2020-11-24 11:20:53 +0100

    Arreglo tipo vehiculo

--- src/Controllers/Venta/ControladorVentaAltaVehiculo.java
+++ src/Controllers/Venta/ControladorVentaAltaVehiculo.java
@@ -5,6 +5,8 @@ import Controllers.AbstractControlador;
 import DAO.VehiculoDAO;
 
 import Models.Vehiculo;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -16,6 +18,7 @@ import javafx.scene.control.TextField;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
@@ -35,6 +38,11 @@ public class ControladorVentaAltaVehiculo extends AbstractControlador {
     @FXML
     Button buttonAceptar;
 
+    @FXML
+    public void initialize(){
+        cargarDesplegables();
+    }
+
     /**
      * Método que sirve para cambiar de una venta a otra dependiendo que boton se ha elegido.
      * @param e
@@ -53,10 +61,10 @@ public class ControladorVentaAltaVehiculo extends AbstractControlador {
             vehiculo.put("NumeroPuertas", tfNPuertas.getText());
             vehiculo.put("Precio", tfPrecio.getText());
             vehiculo.put("FechaEntrada", dpFechaEntrada.getValue().toString());
-            vehiculo.put("numeroRuedas",tfNRuedas.getText());
-            vehiculo.put("fechaSalida",dpFechaSalida.getValue().toString());
-            vehiculo.put("numeroBastidor",tfBastidor.getText());
-            vehiculo.put("tipoVehiculo",cbTipoVehiculo.getValue());
+            vehiculo.put("NumeroRuedas",tfNRuedas.getText());
+            vehiculo.put("FechaSalida",dpFechaSalida.getValue().toString());
+            vehiculo.put("NumeroBastidor",tfBastidor.getText());
+            vehiculo.put("TipoVehiculo",cbTipoVehiculo.getValue());
 
 
             Vehiculo v = new Vehiculo(vehiculo);
@@ -70,5 +78,17 @@ public class ControladorVentaAltaVehiculo extends AbstractControlador {
 
         ControladorVenta co = pane.getController();
         co.setMiApp(miApp);
+    }
+
+    /**
+     * Método que rellena los desplegables ChoiceBox.
+     */
+    public void cargarDesplegables(){
+        ArrayList<String> tipoVehiculos = new ArrayList<>();
+        tipoVehiculos.add("Coche");
+        tipoVehiculos.add("Motocicleta");
+        tipoVehiculos.add("Ciclomotor");
+        ObservableList<String> list = FXCollections.observableArrayList(tipoVehiculos);
+        cbTipoVehiculo.setItems(list);
     }
 }
---
+++ src/Models/Tarea.java
@@ -0,0 +1,7 @@
+package Models;
+
+public class Tarea {
+
+    private String nombre;
+    private String estado;
+}
